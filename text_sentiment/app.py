946d3d022a1845e696f0527b3cca6856d008d129
ChrisCintron <Chriscintron502@gmail.com>
2017-10-11 22:30:41 -0500

    Docstrings,Codebase,Testfile Update

--- text_sentiment/app.py
+++ text_sentiment/app.py
@@ -1,13 +1,24 @@
 import sqlite3
 import os
+from collections import Counter
 
+"""
+TODO:
+1.Maybe start lines_at_a_time variable at line_generator method
+This would insure the object stay roughly the same size
+while being passed through methods. Would possbily mean less generators.
 
+2.Does not properly tally the number of words in testfile when lines_at_a_time
+is set to = 1. Edit: Wrongly assumed dict.update() adds values together. Adding
+Counters is the right way to add them.
+"""
 class FileAnalyzer(object):
     """Prepares data for SentimentAnalyzer"""
     def __init__(self,file_path=None):
         self.file_path = file_path
         self.textfile = None
 
+        self.main()
 
     def line_generator(self):
         """Opens file and generates lines"""
@@ -27,30 +38,51 @@ class FileAnalyzer(object):
 
         Possibly another way of doing it..
         #filtered_list = tuple(filter(str.isalpha,words))
-
-
         """
         for line in line_gen:
+
+            line = line.lower()
+            line = line.replace('.', '')
+            line = line.replace('\'', ' ')
 
             words = tuple(line.split(' '))
             accepted_list = tuple(word for word in words if str.isalpha(word))
             rejected_list = tuple(word for word in words if not str.isalpha(word))
 
-            yield accepted_list,rejected_list
+            yield accepted_list
 
-    def count_words(self,listofwords):
-        """Counts the number of words in text"""
-        pass
+    def word_count_generator(self,filtered_list,lines_at_a_time=1):
+        """Counts the number of words in text
+        lines_at_a_time = Number of lines you want handled at one time
+        Iterate through each line and yield the maximum lines specified
+        """
+        word_counter = 0
+        bulk_words = ()
+        i = 0
+        for line in filtered_list:
+            i += 1
+            bulk_words += line
+            if i >= lines_at_a_time:
+                c = Counter(bulk_words)
+                yield c
+                bulk_words = ()
+                i = 0
 
-    def run(self):
+        if bulk_words:
+            c = Counter(bulk_words)
+            yield c
+
+    def tally_word_count(self,wordcounts):
+        word_bulk = Counter()
+        for wordcount in wordcounts:
+            word_bulk += wordcount
+        print(word_bulk)
+
+    def main(self):
         l = self.line_generator()
         p = self.word_parsing_generator(l)
-
-        #print(next(p))
-        #print(next(p))
-
-
-
+        c = self.word_count_generator(p,lines_at_a_time=10)
+        t = self.tally_word_count(c)
 
 class DBLookup(object):
     """Connects the wordbank.db"""
@@ -73,16 +105,6 @@ class SentimentAnalyzer(object):
 def main():
     testdoc = './text_sentiment/tests/fixtures/testfile.txt'
     f = FileAnalyzer(file_path=testdoc)
-    f.run()
-    #line_gen = f.line_generator()
-    #print(next(line))
-    #f.parse_words(line_gen)
-    #f.parse_words(line_gen)
-    #f.parse_words(line_gen)
-    #f.parse_words(line_gen)
-
-    #print(next(line))
-
 
 if __name__ == '__main__':
     main()
