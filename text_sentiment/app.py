5fd7fbba7c20b7f6f3d18238adeffa46e0734888
ChrisCintron <Chriscintron502@gmail.com>
2017-10-12 13:08:32 -0500

    Touchup Docstrings and code readability

--- text_sentiment/app.py
+++ text_sentiment/app.py
@@ -4,13 +4,7 @@ from collections import Counter
 
 """
 TODO:
-1.Maybe start lines_at_a_time variable at line_generator method
-This would insure the object stay roughly the same size
-while being passed through methods. Would possbily mean less generators.
 
-2.Does not properly tally the number of words in testfile when lines_at_a_time
-is set to = 1. Edit: Wrongly assumed dict.update() adds values together. Adding
-Counters is the right way to add them.
 """
 class FileAnalyzer(object):
     """Prepares data for SentimentAnalyzer"""
@@ -19,21 +13,28 @@ class FileAnalyzer(object):
         self.chunk_size = chunk_size
         self.textfile = None
 
-        #Generators
-        self.line_gen = self.line_generator()
-        self.chunk_gen = self.read_in_chunks(self.line_gen,self.chunk_size)
-
-
-
-    def line_generator(self):
-        """Opens file and generates lines"""
-        with open(self.file_path, 'r') as infile:
+    def open_file(self,file_path=None):
+        """Opens file and generates lines
+        Type:
+            Generator
+        Args:
+            file_path = Path to text file
+        Notes:
+            Accepts plain text files
+        """
+        with open(file_path, 'r') as infile:
             for line in infile:
                 line = line.strip('\n')
                 yield line
 
     def read_in_chunks(self,line_gen=None,chunk_size=None):
-        """Read file by specified number of lines"""
+        """Package lines in file into specific chunk size, return the chunk
+        Type:
+            Generator
+        Args:
+            line_gen = String Generator of lines of file
+            chunk_size = Number of lines from file wanted in each chunk
+        """
         i,text_chunk = 0,str()
         for line in line_gen:
             i += 1
@@ -44,17 +45,15 @@ class FileAnalyzer(object):
         if text_chunk:
             yield text_chunk
 
-    def word_parsing_generator(self,chunk_gen=None):
-        """Finds individual words from text
-        Checks if all chars in word are alphabetical.
-
-        line in line_gen = "Test I am1 a line"
-        words = ('Test', 'I', 'am', 'a', 'line')
-        accepted_list = ('Test', 'I', 'a', 'line')
-        rejected_list = ('am1',)
-
-        Possibly another way of doing it..
-        #filtered_list = tuple(filter(str.isalpha,words))
+    def parse_words(self,chunk_gen=None):
+        """Parse words in chunk, return the accepted words
+        Type:
+            Generator
+        Args:
+            chunk_gen = Tuple Generator of chunks from file
+        Notes:
+            Possibly another way of doing it >>
+            filtered_list = tuple(filter(str.isalpha,words))
         """
         for line in chunk_gen:
 
@@ -68,39 +67,28 @@ class FileAnalyzer(object):
 
             yield accepted_list
 
-    def word_count_generator(self,filtered_list=None,chunk_size=None):
-        """Counts the number of words in text
-        lines_at_a_time = Number of lines you want handled at one time
-        Iterate through each line and yield the maximum lines specified
+    def count_words(self,parsed_chunks=None):
+        """Counts number of words in text, return word counts
+        Type:
+            Generator
+        Args:
+            parsed_chunks = Tuple Generator of parsed words
+            Example:
+                parsed_chunks = ('aword','bword','cword')
         """
-        word_counter = 0
-        bulk_words = ()
-        i = 0
-        for line in filtered_list:
-            i += 1
-            bulk_words += line
-            if i >= chunk_size:
-                c = Counter(bulk_words)
-                yield c
-                bulk_words = ()
-                i = 0
-
-        if bulk_words:
-            c = Counter(bulk_words)
-            yield c
-
-    def tally_word_count(self,wordcounts):
-        word_bulk = Counter()
-        for wordcount in wordcounts:
-            word_bulk += wordcount
-        print(word_bulk)
+        text_count = Counter()
+        for chunk in parsed_chunks:
+            text_count += Counter(chunk)
+        #DEBUG
+        print(text_count)
 
     def main(self):
-        self.line_gen = self.line_generator()
-        #p = self.word_parsing_generator(l)
-        #c = self.word_count_generator(p,lines_at_a_time=10)
-        #t = self.tally_word_count(c)
-        self.read_in_chunks()
+        """Executes analysis of file"""
+        f = self.open_file(file_path=self.file_path)
+        chunks = self.read_in_chunks(line_gen=f,chunk_size=self.chunk_size)
+        parsed_chunks = self.parse_words(chunk_gen=chunks)
+        counts = self.count_words(parsed_chunks=parsed_chunks)
+
 
 class DBLookup(object):
     """Connects the wordbank.db"""
@@ -124,6 +112,7 @@ def main():
     print("Main Starting")
     testdoc = './text_sentiment/tests/fixtures/testfile.txt'
     f = FileAnalyzer(file_path=testdoc,chunk_size=2)
+    f.main()
 
 if __name__ == '__main__':
     main()
