8e6eb3b154e25a0b7aa2cc5620126f8b11bd145b
ChrisCintron <Chriscintron502@gmail.com>
2017-10-11 19:40:56 -0500

    Create word_parsing_generator method

--- text_sentiment/app.py
+++ text_sentiment/app.py
@@ -8,6 +8,7 @@ class FileAnalyzer(object):
         self.file_path = file_path
         self.textfile = None
 
+
     def line_generator(self):
         """Opens file and generates lines"""
         with open(self.file_path, 'r') as f:
@@ -15,13 +16,41 @@ class FileAnalyzer(object):
                 line = line.strip('\n')
                 yield line
 
-    def parse_words(self,file):
-        """Finds individual words from text"""
-        pass
+    def word_parsing_generator(self,line_gen):
+        """Finds individual words from text
+        Checks if all chars in word are alphabetical.
+
+        line in line_gen = "Test I am1 a line"
+        words = ('Test', 'I', 'am', 'a', 'line')
+        accepted_list = ('Test', 'I', 'a', 'line')
+        rejected_list = ('am1',)
+
+        Possibly another way of doing it..
+        #filtered_list = tuple(filter(str.isalpha,words))
+
+
+        """
+        for line in line_gen:
+
+            words = tuple(line.split(' '))
+            accepted_list = tuple(word for word in words if str.isalpha(word))
+            rejected_list = tuple(word for word in words if not str.isalpha(word))
+
+            yield accepted_list,rejected_list
 
     def count_words(self,listofwords):
         """Counts the number of words in text"""
         pass
+
+    def run(self):
+        l = self.line_generator()
+        p = self.word_parsing_generator(l)
+
+        #print(next(p))
+        #print(next(p))
+
+
+
 
 class DBLookup(object):
     """Connects the wordbank.db"""
@@ -44,7 +73,16 @@ class SentimentAnalyzer(object):
 def main():
     testdoc = './text_sentiment/tests/fixtures/testfile.txt'
     f = FileAnalyzer(file_path=testdoc)
-    result = f.line_generator()
+    f.run()
+    #line_gen = f.line_generator()
+    #print(next(line))
+    #f.parse_words(line_gen)
+    #f.parse_words(line_gen)
+    #f.parse_words(line_gen)
+    #f.parse_words(line_gen)
+
+    #print(next(line))
+
 
 if __name__ == '__main__':
     main()
