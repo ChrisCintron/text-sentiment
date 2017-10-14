a60c47b629690c734056b685cf5e69b15c513cb0
ChrisCintron <Chriscintron502@gmail.com>
2017-10-13 19:47:56 -0500

    Add more to tests

--- text_sentiment/tests/test_app.py
+++ text_sentiment/tests/test_app.py
@@ -4,22 +4,22 @@ import pytest
 
 #For later use
 """
+@pytest.fixture(scope="class")
+def file_class(): #FileAnalyzer class
+    fa = FileAnalyzer(file_path=TEST_DOC)
+    yield fa
+"""
+"""
 @pytest.fixture(scope="module")
-def specific_func_needed():
+def db(): #DBLookup class
     pass #Initiate
     v = 0
     yield v
     pass #close
-
-@pytest.make.parametrize("test_input, text_output",
-                        [
-                            (),
-                            (),
-                            (),
-                        ])
 """
 
-class TestFileAnalyzer(object):
+class TestFileAnalyzer():
+    @pytest.mark.skip
     def test_general(self):
         f = FileAnalyzer(file_path=TEST_DOC)
         line_gen = f._openfile()
@@ -27,3 +27,20 @@ class TestFileAnalyzer(object):
         gen2 = f.clean(gen1)
         for i in f.countwords(gen2):
             print(i)
+
+    def test_openfile(self):
+        f = FileAnalyzer(file_path=TEST_DOC)
+        line_gen = f._openfile()
+        output = list(line_gen)
+        assert len(output) == 7
+        assert output[0] == 'This is line one.'
+
+    def test_makechunk(self):
+        f = FileAnalyzer(file_path=TEST_DOC)
+        line_gen = f._openfile()
+        dirtychunks = f.makechunk(line_gen,3)
+        output = list(dirtychunks)
+        assert len(output) == 3
+        assert output[0] == 'This is line one. My second line. Third line is the charm.'
+
+    
