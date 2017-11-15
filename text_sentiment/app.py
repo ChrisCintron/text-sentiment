09550390156e81c3fefc6bfaf9b7ef55d07f0563
ChrisCintron <Chriscintron502@gmail.com>
2017-11-14 22:12:33 -0600

    Rework test file and toss some methods out

--- text_sentiment/app.py
+++ text_sentiment/app.py
@@ -46,41 +46,32 @@ class Database(object):
 
     def query(self, table=None, word=None):
         """Query individual word for value in specified table"""
-        table = self.metadata.tables[table]
-        row = self.session.query(table).filter_by(word=word).first()
+        table_schema = self.metadata.tables[table]
+        row = self.session.query(table_schema).filter_by(word=word).first()
         try:
-            return {word:row.value}
+            return (table,word,row.value)
         except AttributeError:
-            return {word:0}
-
-class NumberCruncher:
-    """Helper that contains analysis tools"""
-    def wordcount(content):
-        """Counts words in list or generator"""
-        totalcount = Counter()
-        if isinstance(content,list): #Catches one liners to work with loop
-            content = [content]
-        for line in content:
-            count = Counter(line)
-            totalcount.update(count)
-        return totalcount
+            return (table,word,0)
 
 class TextSentiment(object):
     def __init__(self,file_path=None,content=None,db_path=None):
-        self.data = defaultdict(dict) #Data container for export
-        self.metrics = {}
-        self.metrics['totaluniquewordcount'] = 0
-
-        self.content = content
+        self.file_path = file_path
+        self.content = [content]
         self.db_path = db_path
 
-        self.db = Database(db_path=db_path) #Compose database connection
         self.filters = Filters()
+        self.db = Database(db_path=db_path) #Compose database connection
+
+        self.data = {} #Data container for export
+        #self.metrics = {}
+        #self.tables = self.db.metadata.tables
+        #self.metrics['totalwords'] = 0
+        #self.metrics['sentimentvalue'] = 0
+        #self.metrics['foundwords'] = 0
 
         if file_path:
             self.content = self._openfile(file_path)
-
-        self.filtered_content = self._filter(self.content)
+        self.content = self._filter(self.content)
 
     def _openfile(self,file_path):
         """Open plain text file and generate lines"""
@@ -94,135 +85,31 @@ class TextSentiment(object):
             filtered_line = self.filters.filter(line)
             yield filtered_line
 
-    def _createlabels(self,labels,value=None):
-        """Populate metric container with labels"""
-        for label in labels:
-            self.metrics[label] = value
-
-    def query(self,table=None,word=None):
+    def _query(self,table=None,word=None):
         """Search db table for word"""
         return self.db.query(table=table,word=word)
 
-    def _updateWord(self,word,count):
-        """Updates that occur on every word iteration"""
-        self.data[word] = {}
-        self.data[word].update(frequency=count,tables={})
-        self.metrics['totaluniquewordcount'] += 1
+    def wordcount(self, content):
+        """Counts words in iterable"""
+        totalcount = Counter()
+        for line in content:
+            count = Counter(line)
+            totalcount.update(count)
+        return totalcount
 
-    def _updateTable(self,word,count,table):
-        """Updates that occur on every table iteration"""
-        query = self.query(table=table,word=word)
-        self.data[word]['tables'].update({table: {}})
-        self.data[word]['tables'][table].update({'value': query[word]})
-        self.data[word]['tables'][table].update({'total_value': count*query[word]})
+    
 
     def run(self):
-        tables = [table for table in self.db.metadata.tables]
-        wordcount = NumberCruncher.wordcount(self.filtered_content)
-        for word,count in wordcount.items():
-            self._updateWord(word,count)
-            for table in tables:
-                self._updateTable(word,count,table)
-    """
-    def set_uniquewordcount(self):
-        results = len(self.shared_dict['words'])
-        self.shared_dict['metrics']['uniquewordcount'] = results
-        return results
-    """
-    """
-    def set_acceptedwordcount(self):
-        results = 0
-        for value in self.shared_dict['words'].values():
-            if value['db_value']:
-                results += value['frequency']
-        self.shared_dict['metrics']['acceptedwordcount'] = results
-        return results
-
-    def set_accepteduniquewordcount(self):
-        results = 0
-        for value in self.shared_dict['words'].values():
-            if value['db_value']:
-                results += 1
-        self.shared_dict['metrics']['accepteduniquewordcount'] = results
-        return results
-
-    def set_rejecteduniquewordcount(self):
-        results = 0
-        for value in self.shared_dict['words'].values():
-            if not value['db_value']:
-                results += 1
-        self.shared_dict['metrics']['rejecteduniquewordcount'] = results
-        return results
-
-    def set_rejectedwordcount(self):
-        results = 0
-        for value in self.shared_dict['words'].values():
-            if not value['db_value']:
-                results += value['frequency']
-        self.shared_dict['metrics']['rejectedwordcount'] = results
-        return results
-
-    def set_highlowratings(self,metric):
-        highest_rating,lowest_rating = 0,10
-        for key,value in self.shared_dict['words'].items():
-            value = value[metric]
-            if value:
-                if value > highest_rating:
-                    highest_word = key
-                    highest_rating = value
-                if value < lowest_rating:
-                    lowest_word = key
-                    lowest_rating = value
-
-        self.shared_dict['metrics']['highest_pair'] = (highest_word,highest_rating)
-        self.shared_dict['metrics']['lowest_pair'] = (lowest_word,lowest_rating)
-
-    def set_word_totalvalue(self):
-        for key,value in self.shared_dict['words'].items():
-            frequency = self.shared_dict['words'][key]['frequency']
-            db_value = self.shared_dict['words'][key]['db_value']
-            results = {"totalvalue":frequency*db_value}
-            self.shared_dict['words'][key].update(results)
-        return results
-
-    def set_word_dbvalue(self):
-        combined_dict = self.query(self.shared_dict['words'].keys())
-        for key,value in combined_dict.items():
-            self.shared_dict['words'][key].update({"db_value":value})
-
-    def set_sentimentvalue(self):
-        totalvalue = 0
-        totalfrequency = self.shared_dict['metrics']['acceptedwordcount']
-        for value in self.shared_dict['words'].values():
-            totalvalue += value['totalvalue']
-        sv = round(totalvalue / totalfrequency,5)
-        self.shared_dict['metrics'].update({'sentimentvalue':sv})
-        return sv
-    """
-    """
-    def run(self):
-        self.create_data_structure(self.metrics,self.shared_dict)
-        self.extractcontent(self.content,self.shared_dict)
-        self.set_word_dbvalue()
-        self.set_word_totalvalue()
-
-        self.set_wordcount()
-        self.set_uniquewordcount()
-        self.set_acceptedwordcount()
-        self.set_accepteduniquewordcount()
-        self.set_rejectedwordcount()
-        self.set_rejecteduniquewordcount()
-        self.set_highlowratings('db_value')
-        self.set_sentimentvalue()
-        print(json.dumps(self.shared_dict, indent=4))
-        return json.dumps(self.shared_dict)
+        pass
+        #self.wordcounts = self.wo
+        #for word in
 
 
-    def __getitem__(self,key):
-        return self.data.__dict__[key]
 
-    def __call__(self):
-        return self.data.__dict__
+
+        #print(json.dumps(self.shared_dict, indent=4))
+        #return json.dumps(self.shared_dict)
+
 
 def myparser():
     parser = argparse.ArgumentParser()
@@ -258,14 +145,6 @@ def main():
         print("\nWordFrequency: ",ts.data.wordcount)
         print("\nFoundInDB: ",ts.data.wordsfound)
         print("\nnotFoundinDB: ",ts.data.wordsnotfound)
-
-def main_test():
-    ts = TextSentiment(TEST_DOC,DB_PATH)
-    ts.run()
-    """
-
-def main():
-    pass
 
 if __name__ == '__main__':
     #Run pytest for now.
--- text_sentiment/tests/test_app.py
+++ text_sentiment/tests/test_app.py
@@ -1,90 +1,122 @@
 import pytest
 import types
-
-#context file adjust our working directory in order to access app
-from context import app
+from context import app #context file adjust our working directory in order to access app
 from app import *
 
+class Test_Filters:
+    @pytest.fixture(autouse=True)
+    def filterobj(self):
+        self.line = 'Cont1ain filt2ers us1ed to filter th103e con*tents from textfile'
+        yield Filters()
 
-@pytest.fixture()
-def fileobj():
-    yield FileObj(file_path=TEST_DOC,)
+    def test_init(self,filterobj):
+        """Split has to be the last function to be called"""
+        assert filterobj.order.pop() == 'split'
 
-class Test_FileObj:
-    def test_init(self):
-        fileobj = FileObj(file_path=TEST_DOC,)
-        assert fileobj
+    def test_remove_badchars(self,filterobj):
+        line = 'Cont1ain filt2ers us1ed to filter th103e con*tents from textfile'
+        output_correct = 'Contain filters used to filter the contents from textfile'
+        output = filterobj.remove_badchars(line)
+        assert output == output_correct
 
-    def test_openfile(self,fileobj):
-        content = fileobj.openfile()
-        assert isinstance(content,types.GeneratorType)
+    def test_make_lowercase(self,filterobj):
+        line = 'ContAiN fIlterS uSeD to fiLTer thE contenTS fRom texTFILE'
+        output_correct = 'contain filters used to filter the contents from textfile'
+        output = filterobj.make_lowercase(line)
+        assert output == output_correct
 
-    def test_filter(self,fileobj):
-        content = fileobj.openfile()
-        content = fileobj.filter(content)
-        assert isinstance(content,types.GeneratorType)
+    def test_split(self,filterobj):
+        line = 'Contain filters used to filter the contents'
+        output_correct = ['Contain','filters','used','to','filter','the','contents']
+        output = filterobj.split(line)
+        assert output == output_correct
 
-    def test_run(self,fileobj):
-        fileobj.run()
-        assert isinstance(fileobj.content,types.GeneratorType)
+    def test_filter(self,filterobj):
+        line = 'Cont1*7ain f@#9ILTErs us9]ed TO fIltER the+ ContenTS'
+        output_correct = ['contain','filters','used','to','filter','the','contents']
+        output = filterobj.filter(line)
+        assert output == output_correct
 
 @pytest.fixture()
 def db():
     yield Database(db_path=DB_PATH)
 
 class Test_Database:
-    def test_init(self):
-        db = Database(db_path=DB_PATH)
+    def test_init(self,db):
+        output_correct = ['Warriner-English','labMTwords-English']
+        output = [table for table in db.metadata.tables]
+        assert set(output_correct).issubset(output)
 
     def test_query(self,db):
         table = 'Warriner-English'
-        #table = db.metadata.tables[table_name]
-        #type(db.metadata.tables['Warriner-English'])
-        assert db.query(table=table, word='love') == {'love': 8}
-        assert db.query(table=table, word='rest') == {'rest': 7.86}
-        assert db.query(table=table, word='brute') == {'brute': 3.48}
+        assert db.query(table=table, word='love') == (table, 'love', 8)
+        assert db.query(table=table, word='rest') == (table, 'rest', 7.86)
+        assert db.query(table=table, word='grave') == (table, 'grave', 2.4)
+        assert db.query(table=table, word='132s') == (table, '132s', 0)
+        assert db.query(table=table, word='x2') == (table, 'x2', 0)
+        assert db.query(table=table, word='yelack') == (table, 'yelack', 0)
 
-        assert db.query(table=table, word='132s') == {'132s': 0}
-        assert db.query(table=table, word='x2') == {'x2': 0}
-        assert db.query(table=table, word='a') == {'a': 0}
+        table = 'labMTwords-English'
+        assert db.query(table=table, word='love') == (table, 'love', 8.42)
+        assert db.query(table=table, word='rest') == (table, 'rest', 7.18)
+        assert db.query(table=table, word='grave') == (table, 'grave', 2.56)
+        assert db.query(table=table, word='132s') == (table, '132s', 0)
+        assert db.query(table=table, word='x2') == (table, 'x2', 0)
+        assert db.query(table=table, word='yelack') == (table, 'yelack', 0)
+
 
 @pytest.fixture()
 def ts():
     yield TextSentiment(file_path=TEST_DOC,db_path=DB_PATH)
 
 class Test_TextSentiment:
-    def test_init(self):
-        labels = ['metrics','words']
-        ts = TextSentiment(file_path=TEST_DOC,db_path=DB_PATH)
-        assert ts.data == {}
+    #@pytest.mark.skip()
+    def test_init(self,ts):
+        pass
 
-    @pytest.mark.skip
-    def test_createlabels(self,ts):
-        data = {}
-        labels = {'metrics','words'}
-        results = ts._createlabels(labels,value={})
-        print(results)
+    #@pytest.mark.skip()
+    def test_openfile(self,ts):
+        file_path = TEST_DOC
+        output_1_correct = 'My second line.'
+        output_2_correct = 'Third line is the charm.'
+        output_3_correct = 'Fourth one fits the bill.'
+        output = ts._openfile(file_path)
+        assert next(output) == output_1_correct
+        assert next(output) == output_2_correct
+        assert next(output) == output_3_correct
 
-    def test_run(self,ts,db):
-        ts.run()
-        print(ts.data)
+    #@pytest.mark.skip()
+    def test_filter(self,ts):
+        content = ['My Second Line']
+        output_correct = ['my','second','line']
+        output = ts._filter(content)
+        assert next(output) == output_correct
 
-class Test_NumberCruncher:
+    #@pytest.mark.skip
+    def test_query(self,ts):
+        """Test single word query on both tables"""
+        tables = ['Warriner-English','labMTwords-English']
+        word = 'love'
+        output_correct = (tables[0], 'love', 8)
+        output = ts._query(table=tables[0], word=word)
+        assert output == output_correct
 
-    @pytest.fixture(autouse=True)
-    def nc(self):
-        self.line = "My word My word does not equal one".split(' ')
-        yield NumberCruncher
+        output_correct = (tables[1], 'love', 8.42)
+        output = ts._query(table=tables[1], word=word)
+        assert output == output_correct
 
-    def test_wordcount(self,nc,ts):
-        wordcounts = nc.wordcount(ts.filtered_content)
-        #assert wordcounts['i'] == 30
+    def test_wordcount(self,ts):
+        """Tests specific wordcount and actually returned data structure"""
+        word = 'is'
+        value = 1
+        content = [['my','second','line',
+                    'third','line','is','the','charm',
+                    'fourth','one','fits','the','bill',]]
+        output_correct = value
+        output = ts.wordcount(content)[word]
+        assert output == output_correct
 
-@pytest.mark.skip
-def test_main():
-    string = "I love having fun!"
-    data = TextSentiment(db_path=DB_PATH, content=string)
-
-    path = TEST_DOC
-    data = TextSentiment(db_path=DB_PATH, file_path=TEST_DOC)
-    data.sentimentvalue
+        content = [['one','two','two','three','three','three']]
+        output_correct = Counter({'one':1,'two':2,'three':3})
+        output = ts.wordcount(content)
+        assert output == output_correct
